37210f505cc3ecce9cbf5b72aba96fa0d19b9bbe
Dumont Augustin <augustindmt@gmail.com>
2020-01-08 17:02:25 +0100

    finish svh background + timeline horizontal scroll

---
+++ assets/js/functions.js
@@ -0,0 +1,16 @@
+// $(document).ready(function () {
+//     var docWidth = $('body').width(),
+//         slidesWidth = $('ul').width(),
+//         $ul = $('ul');
+
+//     $(document).on('mousemove', function (e) {
+//         var mouseX = e.pageX,
+//             offset = mouseX / docWidth * slidesWidth - mouseX / 2;
+//         console.log("mouseX");
+
+//         $ul.css({
+//             '-webkit-transform': 'translate3d(' + -offset + 'px,0,0)',
+//             'transform': 'translate3d(' + -offset + 'px,0,0)'
+//         });
+//     });
+// });
