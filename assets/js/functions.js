85b2640e18b5f47d5a3471cf9336af9d35ef34b2
Dumont Augustin <augustindmt@gmail.com>
2020-01-10 16:57:17 +0100

    add custom color and skills

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,16 +1,45 @@
-// $(document).ready(function () {
-//     var docWidth = $('body').width(),
-//         slidesWidth = $('ul').width(),
-//         $ul = $('ul');
+var blue = document.getElementById("blue");
+var red = document.getElementById("red");
+var green = document.getElementById("green");
+var pastel = document.getElementById("pastel");
 
-//     $(document).on('mousemove', function (e) {
-//         var mouseX = e.pageX,
-//             offset = mouseX / docWidth * slidesWidth - mouseX / 2;
-//         console.log("mouseX");
+blue.addEventListener("click", () => {
+    document.documentElement.style.setProperty("--color-lighter", "#aeccdc");
+    document.documentElement.style.setProperty("--color-light", "#d4e7ef");
+    document.documentElement.style.setProperty("--color-normal", "#b3cede");
+    document.documentElement.style.setProperty("--color-regular", "#80a4c6");
+    document.documentElement.style.setProperty("--color-dark", "#5b7dac");
+    document.documentElement.style.setProperty("--color-darker", "#5b79ab");
+    document.documentElement.style.setProperty("--color-flash", "#68d7f4");
+})
 
-//         $ul.css({
-//             '-webkit-transform': 'translate3d(' + -offset + 'px,0,0)',
-//             'transform': 'translate3d(' + -offset + 'px,0,0)'
-//         });
-//     });
-// });
+
+red.addEventListener("click", () => {
+    document.documentElement.style.setProperty("--color-lighter", "#F5EA91");
+    document.documentElement.style.setProperty("--color-light", "#F8D462");
+    document.documentElement.style.setProperty("--color-normal", "#f5b636 ");
+    document.documentElement.style.setProperty("--color-regular", "#F09536");
+    document.documentElement.style.setProperty("--color-dark", "#891525");
+    document.documentElement.style.setProperty("--color-darker", "#D55330");
+    document.documentElement.style.setProperty("--color-flash", "#EC3E37");
+})
+
+green.addEventListener("click", () => {
+    document.documentElement.style.setProperty("--color-lighter", "#D9E8B91");
+    document.documentElement.style.setProperty("--color-light", "#B2DD58");
+    document.documentElement.style.setProperty("--color-normal", "#4EA235");
+    document.documentElement.style.setProperty("--color-regular", "#3A9135");
+    document.documentElement.style.setProperty("--color-dark", "#319468");
+    document.documentElement.style.setProperty("--color-darker", "#2F715B");
+    document.documentElement.style.setProperty("--color-flash", "#53E799");
+})
+
+pastel.addEventListener("click", () => {
+    document.documentElement.style.setProperty("--color-lighter", "#FDFCEF");
+    document.documentElement.style.setProperty("--color-light", "#FDF3D0");
+    document.documentElement.style.setProperty("--color-normal", "#FCD0DB");
+    document.documentElement.style.setProperty("--color-regular", "#D2DAFD");
+    document.documentElement.style.setProperty("--color-dark", "#A54E97");
+    document.documentElement.style.setProperty("--color-darker", "#502A59");
+    document.documentElement.style.setProperty("--color-flash", "#A598EC");
+})
