e8393a39ecf84cfc7e11a5565120ddefae70ab59
Dumont Augustin <augustindmt@gmail.com>
2020-01-21 19:26:09 +0100

    finish

---
+++ mail.php
@@ -0,0 +1,35 @@
+<?php
+
+
+$name = "";
+$email = "";
+$msg = "";
+$counter = 0;
+
+
+if (isset($_POST['submit'])) {
+
+    if (empty($_POST["user_name"])) {
+        $counter++;
+    } else {
+        $name = $_POST["user_name"];
+    }
+
+    if (empty($_POST["user_mail"])) {
+        $counter++;
+    } else {
+        $email = $_POST["user_mail"];
+    }
+
+    if (empty($_POST["user_message"])) {
+        $counter++;
+    } else {
+        $msg_body = $_POST["user_message"];
+    }
+
+
+    if ($counter == 0) {
+        $msg =  $msg_body . "\n" . $name . "\n" . $email;
+        mail("contact@augustindumont.be", "Vous avez un message", $msg, "From: noreply@augustindumont.be");
+    }
+}
